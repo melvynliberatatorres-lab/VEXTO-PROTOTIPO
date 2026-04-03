bea5dd364bf01ae82bccfeefdf52517150b14ee4
melvynliberatatorres-lab <melvynliberatatorres@gmail.com>
2026-04-03 19:45:46 -0400

    Delete vexto directory

--- vexto/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Redirige al punto de entrada público de la aplicación.
-// Esto permite acceder con http://localhost/vexto/ sin mostrar el listado de directorio.
-header('Location: public/index.php');
-exit;
